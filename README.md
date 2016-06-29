97d7792f8672fc46ab42e9edb1470fb52743f52e
Alan Bennett <alan@akb.net>
2016-06-29 10:18:14 -0600

    initial commit of repository, adding the base LAVA installation variant

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lava-docker
 Deploying LAVA in a docker image
+
+base-lava-install/ - This Dockerfile creates a base lava installation on Debian sid.  You will have to configure the server
---
+++ base-lava-install/start.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+postgres-ready () {
+  echo "Waiting for lavaserver database to be active"
+  while (( $(ps -ef | grep -v grep | grep postgres | grep lavaserver | wc -l) == 0 ))
+  do
+    echo -n "."
+    sleep 1
+  done
+  echo 
+  echo "[ ok ] LAVA server ready"
+}
+
+start () {
+  echo "Starting $1"
+  if (( $(ps -ef | grep -v grep | grep $1 | wc -l) > 0 ))
+  then
+    echo "$1 appears to be running"
+  else
+    service $1 start
+  fi
+}
+
+start postgresql
+start apache2
+start lava-server
+
+postgres-ready
