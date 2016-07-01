fbbe2a09eef7539a638fe29af07bdcce64f1dcda
Alan Bennett <alan.bennett@linaro.org>
2016-06-30 18:16:38 -0600

    add a qemu-aarch64 device type and sample job

--- demo-lava-install/add-kvm-to-lava.sh
+++ demo-lava-install/add-kvm-to-lava.sh
@@ -3,8 +3,6 @@
 
 adminuser=admin
 adminpass=admin
-devicename=kvm01
-devicetype=kvm
 
 lavaurl=http://localhost
 
@@ -17,15 +15,26 @@ csrf="csrfmiddlewaretoken="$(cat cookies.txt | grep csrftoken | cut -d$'\t' -f 7
 login=$csrf\&username=$adminuser\&password=$adminpass; echo $login
 curl -b cookies.txt -c cookies.txt -d $login -X POST $lavaurl/admin/login/
 
-# configure device type and device
-# Add new device type "kvm"
-## http://localhost/admin/lava_scheduler_app/devicetype/add/
+devicename=kvm01
+devicetype=kvm
+# add device type
 csrf="csrfmiddlewaretoken="$(cat cookies.txt | grep csrftoken | cut -d$'\t' -f 7); echo $csrf
 createdevicetype=$csrf\&name=$devicetype\&display=on\&health_frequency=24\&_save=Save\&health_denominator=0
 curl -b cookies.txt -c cookies.txt -d $createdevicetype -X POST $lavaurl/admin/lava_scheduler_app/devicetype/add/
 
-# Add new device "kvm01" of device type "qemu"
-## http://localhost/admin/lava_scheduler_app/device/add/
+# add device
+csrf="csrfmiddlewaretoken="$(cat cookies.txt | grep csrftoken | cut -d$'\t' -f 7); echo $csrf
+createdevice=$csrf\&hostname=$devicename\&device_type=$devicetype\&device_version=1\&status=1\&health_status=0
+curl -b cookies.txt -c cookies.txt -d $createdevice -X POST $lavaurl/admin/lava_scheduler_app/device/add/
+
+devicename=qemu-aarch64-01
+devicetype=qemu-aarch64
+# add device type
+csrf="csrfmiddlewaretoken="$(cat cookies.txt | grep csrftoken | cut -d$'\t' -f 7); echo $csrf
+createdevicetype=$csrf\&name=$devicetype\&display=on\&health_frequency=24\&_save=Save\&health_denominator=0
+curl -b cookies.txt -c cookies.txt -d $createdevicetype -X POST $lavaurl/admin/lava_scheduler_app/devicetype/add/
+
+## Add device
 csrf="csrfmiddlewaretoken="$(cat cookies.txt | grep csrftoken | cut -d$'\t' -f 7); echo $csrf
 createdevice=$csrf\&hostname=$devicename\&device_type=$devicetype\&device_version=1\&status=1\&health_status=0
 curl -b cookies.txt -c cookies.txt -d $createdevice -X POST $lavaurl/admin/lava_scheduler_app/device/add/
