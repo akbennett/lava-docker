a112f8894d52439cc61326e6707f5036e50ffc03
Fathi Boudra <fathi.boudra@linaro.org>
2016-08-06 10:34:31 +0300

    scripts: fix various shell issues/styles/inconsistency
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- add-kvm-to-lava.sh
+++ add-kvm-to-lava.sh
@@ -5,49 +5,48 @@ adminuser=admin
 adminpass=admin
 
 lavaurl=http://localhost
-hostn=`cat /hostname`
+hostn=$(cat /hostname)
 
 #obtain the csrf token
 data=$(curl -s -c cookies.txt $lavaurl/accounts/login/); tail cookies.txt
 
 #login
-csrf="csrfmiddlewaretoken="$(cat cookies.txt | grep csrftoken | cut -d$'\t' -f 7); echo $csrf
+csrf="csrfmiddlewaretoken="$(grep csrftoken cookies.txt | cut -d$'\t' -f 7); echo "$csrf"
 login=$csrf\&username=$adminuser\&password=$adminpass; echo $login
 curl -b cookies.txt -c cookies.txt -d $login -X POST $lavaurl/admin/login/
 
 devicename=kvm01
 devicetype=kvm
 # add device type
-csrf="csrfmiddlewaretoken="$(cat cookies.txt | grep csrftoken | cut -d$'\t' -f 7); echo $csrf
+csrf="csrfmiddlewaretoken="$(grep csrftoken cookies.txt | cut -d$'\t' -f 7); echo "$csrf"
 createdevicetype=$csrf\&name=$devicetype\&display=on\&health_frequency=24\&_save=Save\&health_denominator=0
 curl -b cookies.txt -c cookies.txt -d $createdevicetype -X POST $lavaurl/admin/lava_scheduler_app/devicetype/add/
 
 # add device
-csrf="csrfmiddlewaretoken="$(cat cookies.txt | grep csrftoken | cut -d$'\t' -f 7); echo $csrf
+csrf="csrfmiddlewaretoken="$(grep csrftoken cookies.txt | cut -d$'\t' -f 7); echo "$csrf"
 createdevice=$csrf\&hostname=$devicename\&device_type=$devicetype\&device_version=1\&status=1\&health_status=0\&worker_host=$hostn
 curl -b cookies.txt -c cookies.txt -d $createdevice -X POST $lavaurl/admin/lava_scheduler_app/device/add/
 
 devicename=qemu-aarch64-01
 devicetype=qemu-aarch64
 # add device type
-csrf="csrfmiddlewaretoken="$(cat cookies.txt | grep csrftoken | cut -d$'\t' -f 7); echo $csrf
+csrf="csrfmiddlewaretoken="$(grep csrftoken cookies.txt | cut -d$'\t' -f 7); echo "$csrf"
 createdevicetype=$csrf\&name=$devicetype\&display=on\&health_frequency=24\&_save=Save\&health_denominator=0
 curl -b cookies.txt -c cookies.txt -d $createdevicetype -X POST $lavaurl/admin/lava_scheduler_app/devicetype/add/
 
 ## Add device
-csrf="csrfmiddlewaretoken="$(cat cookies.txt | grep csrftoken | cut -d$'\t' -f 7); echo $csrf
+csrf="csrfmiddlewaretoken="$(grep csrftoken cookies.txt | cut -d$'\t' -f 7); echo "$csrf"
 createdevice=$csrf\&hostname=$devicename\&device_type=$devicetype\&device_version=1\&status=1\&health_status=0\&worker_host=$hostn
 curl -b cookies.txt -c cookies.txt -d $createdevice -X POST $lavaurl/admin/lava_scheduler_app/device/add/
 
 devicename=qemu01
 devicetype=qemu
 # add device type
-csrf="csrfmiddlewaretoken="$(cat cookies.txt | grep csrftoken | cut -d$'\t' -f 7); echo $csrf
+csrf="csrfmiddlewaretoken="$(grep csrftoken cookies.txt | cut -d$'\t' -f 7); echo "$csrf"
 createdevicetype=$csrf\&name=$devicetype\&display=on\&health_frequency=24\&_save=Save\&health_denominator=0
 curl -b cookies.txt -c cookies.txt -d $createdevicetype -X POST $lavaurl/admin/lava_scheduler_app/devicetype/add/
 
 ## Add device
-csrf="csrfmiddlewaretoken="$(cat cookies.txt | grep csrftoken | cut -d$'\t' -f 7); echo $csrf
+csrf="csrfmiddlewaretoken="$(grep csrftoken cookies.txt | cut -d$'\t' -f 7); echo "$csrf"
 createdevice=$csrf\&hostname=$devicename\&device_type=$devicetype\&device_version=1\&status=1\&health_status=0\&is_pipeline="on"\&worker_host=$hostn
 curl -b cookies.txt -c cookies.txt -d $createdevice -X POST $lavaurl/admin/lava_scheduler_app/device/add/
-
--- submittestjob.sh
+++ submittestjob.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 echo "Submit basic kvm job for v1 dispatcher device"
 /tools/submit.py -k /apikey.txt /tools/kvm-basic.json
+
 echo "Submit basic qemu-aarch64 job for v1 dispatcher device"
 /tools/submit.py -k /apikey.txt /tools/kvm-qemu-aarch64.json
 
-#submit a pipeline job
 echo "Submit and wait for v2/pipeline device to complete"
 /tools/submityaml.py -k /apikey.txt -p /tools/qemu.yaml
