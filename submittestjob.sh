b69f2b468ec78e20325ecd749aef6d4b11c24551
Alan <alan@akb.net>
2016-08-17 08:12:01 -0600

    only poll on the last job submission

--- submittestjob.sh
+++ submittestjob.sh
@@ -10,9 +10,9 @@ echo "Submit basic qemu-aarch64 job for v1 dispatcher device"
 submit.py -k apikey.txt kvm-qemu-aarch64.json
 
 echo "Submit and wait for v2/pipeline device to complete"
-submityaml.py -k apikey.txt -p qemu.yaml
+submityaml.py -k apikey.txt qemu.yaml
 
 echo "Submit and wait for v2/pipeline Zephyr qemu-cortex-m3 crypto tests to complete"
-submityaml.py -k apikey.txt -p qemu-cortex-m3-zephyr-crypto-tests-set1.yaml 
-submityaml.py -k apikey.txt -p qemu-cortex-m3-zephyr-crypto-tests-set2.yaml
+submityaml.py -k apikey.txt qemu-cortex-m3-zephyr-crypto-tests-set1.yaml 
+submityaml.py -k apikey.txt qemu-cortex-m3-zephyr-crypto-tests-set2.yaml
 submityaml.py -k apikey.txt -p qemu-cortex-m3-zephyr-crypto-tests-set3.yaml
